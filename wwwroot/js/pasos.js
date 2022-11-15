b45725e8c00633e9579e0123cd75336d96c9ea3e
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-15 11:30:00 -0600

    Vista parcial para insertar paso en _Pasos.cshtml

---
+++ wwwroot/js/pasos.js
@@ -0,0 +1,11 @@
+﻿
+function manejarAgregarPaso() {
+    const indice = editarTareaViewModel.pasos().findIndex(p => p.esNuevo());
+
+    if (indice !== -1) {
+        return;
+    }
+
+    editarTareaViewModel.pasos.push(new pasoViewModel({ modoEdicion: true, realizado: false }));
+    $("[name=txtPasoDescripcion]:visible").focus();
+}
